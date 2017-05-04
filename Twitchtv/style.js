1ae3b002db211db586d27221c8efc62870c2da9c
Adarsh S <4d4r5h.5@gmail.com>
2017-05-04 15:07:12 +0530

    Twitch complete

--- Twitchtv/style.js
+++ Twitchtv/style.js
@@ -1,41 +1,18 @@
 $(document).ready(function(){
+    function twitch(){
+        var url = ["https://api.twitch.tv/kraken/users/","?client_id=dyqe90703142qejdl5d1j8ujyq6mii"];
+        var users = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "habathcx", "RobotCaleb", "noobs2ninjas"];
 
-    function weatherAPI($city, $temp){
-        var url_start = "http://api.openweathermap.org/data/2.5/weather?q=";
-        var uid = "4c7a00a8818a319e3d11c509862a43c2";
-        var url = url_start+$city+"&appid="+uid;
-
-        $.getJSON(url, function(data){
-            var country = data.sys.country;
-            var temperature = data.main.temp;
-            var humidity = data.main.humidity;
-            var wind = data.wind.speed;
-            var feel = data.weather["0"].main;
-            if ($temp=="celcius"){
-                temperature -= 273;
-            }
-            else{
-                temperature = temperature*(9/5)-459.67;
-            }
-            temperature = (temperature).toFixed(0);
-            $("#city_c").text($city+", ");
-            $("#city_d").text(country+", ");
-            $("#city_temp").text(temperature + " degrees");
-            $("#city_val").text(feel);
-            $("#city_humidity").text("Humidity: "+humidity+"  ");
-            $("#city_wind").text("Wind: "+wind);
-        });
-
+        for(var i = 0; i<users.length; i++){
+            $.getJSON(url[0]+ users[i] +url[1], function(data){
+                var content = "";
+                content += "<div class='ind'><div class='row col-xs-12' ><img src='" + data.logo + "' class='icon'><a href='";
+                content += "https://www.twitch.tv/" + data.display_name + "' class='link'  target='_blank'><h2 class='title'>";
+                content += data.display_name + "</h2></a></div><br><p class='para center-block'>" + data.bio + "</p></div>";
+                console.log(content);
+                $(".square").append(content);
+            });
+        }
     }
-
-    $(".submit").on("click", function(){
-        var $city = $("#city").val();
-        if($city===""){
-            alert("Enter a City!!!!!");
-        }
-        else{
-            var $temp = $('input[name=temp]:checked').val();
-            weatherAPI($city, $temp);
-        }
-    });
+    twitch();
 });
